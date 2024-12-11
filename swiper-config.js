6a5d8fec7b22d4ac74864339a619716fdf1e74bc
dadagss <daniel.guimaraess@sempreceub.com>
2024-12-11 17:38:02 -0300

    Update-visual

--- swiper-config.js
+++ swiper-config.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper-container', {
     loop: true, // Permite looping contínuo
     centeredSlides: true, // Centraliza o slide ativo
     slidesPerView: 3, // Mostra 3 slides visíveis ao mesmo tempo
-    spaceBetween: 10, // Espaçamento entre os slides
+    spaceBetween: 30, // Espaçamento entre os slides (gap)
     coverflowEffect: {
         rotate: 0, // Sem rotação
         stretch: 0, // Sem esticamento
